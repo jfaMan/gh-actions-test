6e4d886e2353fd2c1405ed57ebe0a8e2fbae5484
Allan Szewczyk <jfa_nakata@hotmail.com>
2024-09-04 11:27:55 +0900

    update structure

--- download.sh
+++
@@ -1,3 +0,0 @@
-. .env
-
-npx fusionauth theme:download $THEME_ID -k $API_KEY -o $TMP_DIR -h $FUSIONAUTH_URL
--- scripts/upload-theme.sh
+++ scripts/upload-theme.sh
@@ -1,3 +1,5 @@
 . .env
 
+THEME_DIR="../templates/theme"
+
 npx fusionauth theme:upload $THEME_ID -k $API_KEY -i $THEME_DIR -h $FUSIONAUTH_URL
